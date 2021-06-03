f7730516301349bb4c8e0fa85dbd2ed8e745c8df
johnnybcs <johnny@ualberta.ca>
2021-06-02 23:32:54 -0700

    added review rating

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,9 +1,16 @@
 import './Review.css'
+import { Rate } from 'antd';
+import { Row, Col } from 'antd';
+
 
 function Review() {
+    let name = "Johnny Li"
     return (
         <div>
-            
+            <Row>
+                <Col className="reviewName" flex={2.5}>{name}</Col>
+                <Col className="reviewRating" flex={2.5}><Rate allowHalf defaultValue={3.5} /></Col>
+             </Row>  
         </div>
     );
 }
