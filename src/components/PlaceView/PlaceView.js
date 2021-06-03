92aeea33ee159961bc7b9c8147a9a8aac2358f60
Amir Jafarvand <amir.jafarvand@gmail.com>
2021-06-03 16:04:56 -0700

    PlaceView General Layout

--- src/components/PlaceView/PlaceView.js
+++ src/components/PlaceView/PlaceView.js
@@ -1,12 +1,39 @@
-import './PlaceView.css'
-import ReviewList from '../ReviewList/ReviewList'
+import "./PlaceView.css";
+import ReviewList from "../ReviewList/ReviewList";
+import { Row, Col, Typography, Button, Image } from "antd";
+import { PlusOutlined } from "@ant-design/icons";
 
 function PlaceView() {
-    return (
-        <div>
-            <ReviewList />
-        </div>
-    );
+  const { Title } = Typography;
+  return (
+    <div>
+      <Row>
+        <Button
+          type="primary"
+          shape="circle"
+          icon={<PlusOutlined />}
+          size="large"
+        />
+        <Title level={2}>Earls Yaletown</Title>
+      </Row>
+      <Row>
+        <Col span={10}>
+          <Row>
+            <Image
+              width={200}
+              src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+            />
+          </Row>
+          <Row>
+          <Title level={2}>🍔 Restaurant</Title>
+          </Row>
+        </Col>
+        <Col span={14}>
+          <ReviewList />
+        </Col>
+      </Row>
+    </div>
+  );
 }
 
 export default PlaceView;
