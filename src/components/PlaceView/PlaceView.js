ecadb94a86611998815f79ad315d3022fd7effb2
Amir Jafarvand <amir.jafarvand@gmail.com>
2021-06-04 09:20:28 -0700

    added some styling and real image

--- src/components/PlaceView/PlaceView.js
+++ src/components/PlaceView/PlaceView.js
@@ -1,37 +1,62 @@
 import "./PlaceView.css";
 import ReviewList from "../ReviewList/ReviewList";
-import { Row, Col, Typography, Button, Image } from "antd";
+import { Row, Col, Typography, Button, Image, Divider } from "antd";
 import { PlusOutlined } from "@ant-design/icons";
 
 function PlaceView() {
   const { Title } = Typography;
   return (
-    <div>
-      <Row>
+    <div className="container">
+      <Row style={{
+            marginLeft: "20px"
+            
+          }}>
         <Button
           type="primary"
           shape="circle"
-          icon={<PlusOutlined />}
+          icon={
+            <PlusOutlined
+              style={{
+                fontSize: "18px",
+                color: "black",
+                strokeWidth: "100",
+                stroke: "black",
+              }}
+            />
+          }
           size="large"
+          style={{
+            backgroundColor: "lightgrey",
+            borderColor: "white",
+          }}
         />
         <Title level={2}>Earls Yaletown</Title>
       </Row>
-      <Row>
-        <Col span={10}>
-          <Row>
-            <Image
-              width={200}
-              src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
-            />
-          </Row>
-          <Row>
-          <Title level={2}>🍔 Restaurant</Title>
-          </Row>
-        </Col>
-        <Col span={14}>
-          <ReviewList />
-        </Col>
-      </Row>
+      <Divider
+        style={{
+          marginTop: "0",
+          borderWidth: 5,
+        }}
+      />
+      <Col span={10}>
+        <Image
+          width={500}
+          src="https://i.ibb.co/kg11VY4/Screen-Shot-2021-06-04-at-8-54-26-AM.png"
+          style={{ borderRadius: "15px" }}
+        />
+        <Title
+          level={2}
+          style={{
+            margin: "20px"
+            
+          }}
+        >
+          🍔 Restaurant
+        </Title>
+      </Col>
+      <Col span={14}>
+        <ReviewList />
+      </Col>
     </div>
   );
 }
