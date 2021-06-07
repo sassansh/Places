941a7837c3dbdf80fad92ac37b503a6660ece681
Laura Rodgers <>
2021-06-07 11:01:38 -0700

    refactored review vs. reviewlist

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -2,25 +2,14 @@ import './Review.css'
 import { Card, Rate, Col, Row } from 'antd';
 
 function Review(props) {
-    let reviewItems = props.reviews.map((review) => 
-        <div>
-            <li>
-                <Card className="review" size="small" style={{borderRadius: "10px"}}>
-                    <Row>
-                        <Col lg={12} className="reviewer"><h2>{review.reviewer}</h2></Col>
-                        <Col lg={12} className="rating"><Rate allowHalf defaultValue={review.rating} /></Col>
-                    </Row>
-                </Card>  
-            </li>   
-        </div>
-    );
-    return (
-    <div>
-      <h1>{props.reviews.length + " Reviews"}</h1>
-      <ul>{reviewItems}</ul> 
-    </div>
- 
-    );
+    return (<li>
+            <Card className="review" size="small" style={{borderRadius: "10px"}}>
+                <Row>
+                    <Col lg={12} className="reviewer"><h2>{props.review.reviewer}</h2></Col>
+                    <Col lg={12} className="rating"><Rate allowHalf defaultValue={props.review.rating} /></Col>
+                </Row>
+            </Card>
+          </li>);
 }
 
 export default Review;
--- src/components/ReviewList/ReviewList.js
+++ src/components/ReviewList/ReviewList.js
@@ -2,17 +2,22 @@ import './ReviewList.css'
 import Review from '../Review/Review'
 
 function ReviewList() {
-    let reviews = [
+    let reviewsData = [
         { reviewer: "Johnny Li", rating: 3.5},
         { reviewer: "Sassan Shokoohi", rating: 2.5},
-        { reviewer: "Laura Rodgers", rating: 4},
+        { reviewer: "Laura Rodgers", rating: 1},
         { reviewer: "Amir Jafarvand", rating: 5}
     ];
 
+    let reviews = reviewsData.map((reviewData) =>
+      <Review review={reviewData} />
+    );
+
     return (
-        <div>
-            <Review reviews={reviews} />
-        </div>
+      <div>
+        <h1>{reviewsData.length + " Reviews"}</h1>
+        <ul>{reviews}</ul>
+      </div>
     );
 }
 
