b4ad5b6becc807deb55a4e6ecc503e239f40cb6e
Sassan Shokoohi <sassan_shokoohi@me.com>
2021-06-06 21:07:07 -0700

    added simple navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import "./App.css";
+import { Tabs } from "antd";
 import CategoryView from "./components/CategoryView/CategoryView";
 import PlaceView from "./components/PlaceView/PlaceView";
 import "antd/dist/antd.css";
 
+const { TabPane } = Tabs;
+
+function callback(key) {
+  console.log(key);
+}
+
 function App() {
   return (
-    <div>
-      <CategoryView />
-      <PlaceView />
-    </div>
+    <Tabs className="App" defaultActiveKey="1" onChange={callback}>
+      <TabPane tab="Category View" key="1">
+        <CategoryView />
+      </TabPane>
+      <TabPane tab="Place View" key="2">
+        <PlaceView />
+      </TabPane>
+    </Tabs>
   );
 }
 
