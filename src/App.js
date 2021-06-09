eaaaba00ff3f283251d5476be013faa98d3401ba
Sassan Shokoohi <sassan_shokoohi@me.com>
2021-06-08 17:03:26 -0700

    implement basic react router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
 import "./App.css";
-import { Tabs } from "antd";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { Layout, Menu } from "antd";
 import CategoryView from "./components/CategoryView/CategoryView";
 import PlaceView from "./components/PlaceView/PlaceView";
+import { ShopOutlined, TeamOutlined } from "@ant-design/icons";
 import "antd/dist/antd.css";
 
-const { TabPane } = Tabs;
+const { Content, Footer, Sider } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <Tabs defaultActiveKey="1">
-        <TabPane tab="Category View" key="1">
-          <CategoryView />
-        </TabPane>
-        <TabPane tab="Place View" key="2">
-          <PlaceView />
-        </TabPane>
-      </Tabs>
-    </div>
+    <Router basename="/">
+      <Layout>
+        <Sider
+          style={{
+            overflow: "auto",
+            height: "100vh",
+            position: "fixed",
+            left: 0,
+          }}
+        >
+          <div className="logo" />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
+            <Menu.Item key="1" icon={<TeamOutlined />}>
+              <Link to="/">Category View</Link>
+            </Menu.Item>
+            <Menu.Item key="2" icon={<ShopOutlined />}>
+              <Link to="/placeview">Place View</Link>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout" style={{ marginLeft: 200 }}>
+          <Content style={{ margin: "24px 16px 0", overflow: "initial" }}>
+            <Switch>
+              <Route exact path="/">
+                <CategoryView />
+              </Route>
+              <Route exact path="/placeview">
+                <PlaceView />
+              </Route>
+            </Switch>
+          </Content>
+          <Footer style={{ textAlign: "center" }}>
+            Places App ©2021 Created by Team Green 🌱
+          </Footer>
+        </Layout>
+      </Layout>
+    </Router>
   );
 }
 
