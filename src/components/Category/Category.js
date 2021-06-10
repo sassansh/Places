bf73bcde851349fd1abc8b423d34b099ef87f612
Sassan Shokoohi <sassan_shokoohi@me.com>
2021-06-10 12:27:09 -0700

    👌 IMPROVE: Remove props.

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -2,19 +2,19 @@ import "./Category.css";
 import { Card, Col, Row, Avatar } from "antd";
 import { Link } from "react-router-dom";
 
-function Category(props) {
+function Category({ category }) {
   return (
     <Link to="/categoryview">
       <Card style={{ margin: 16 }}>
         <span className="category">
           <Avatar style={{ color: "#f56a00", backgroundColor: "#fde3cf" }}>
-            {props.category.categoryEmoji}
+            {category.categoryEmoji}
           </Avatar>
-          <span className="category-name">{props.category.categoryName}</span>
+          <span className="category-name">{category.categoryName}</span>
         </span>
 
         <span className="num-of-places">
-          {props.category.places.length + " Places"}
+          {category.places.length + " Places"}
         </span>
       </Card>
     </Link>
