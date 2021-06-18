15553372de0820f19e0604ff588e15f00d445b9b
Amir Jafarvand <amir.jafarvand@gmail.com>
2021-06-18 11:53:20 -0700

    create new group

--- client/src/components/CreateGroup/CreateGroup.js
+++ client/src/components/CreateGroup/CreateGroup.js
@@ -1,11 +1,35 @@
 import "./CreateGroup.css";
 
 import { Button, Col, Divider, Form, Input, Row, Typography } from "antd";
-
+import GroupsContext from "../../context/GroupsContext";
+import CurrentGroupIDContext from "../../context/CurrentGroupIDContext";
+import { useContext } from "react";
 import { Link } from "wouter";
 
 function CreateGroup() {
   const { Title } = Typography;
+  const [form] = Form.useForm();
+  const [groups] = useContext(GroupsContext);
+  const [currentGroupID, setCurrentGroupID] = useContext(CurrentGroupIDContext);
+  function addGroup() {
+    let group_id = groups.length + 1;
+    let name = form.getFieldValue("name");
+    let description = form.getFieldValue("description");
+    let avatarURL = form.getFieldValue("logo");
+    if (name === undefined) return;
+    let newGroup = {
+      group_id: group_id,
+      name: name,
+      description: description,
+      avatarURL: avatarURL,
+      numReviews: 0,
+      numMembers: 0,
+    };
+    console.log(currentGroupID);
+    groups.push(newGroup);
+    setCurrentGroupID(group_id);
+    form.resetFields();
+  };
   return (
     <div className="container">
       <Row
@@ -25,22 +49,25 @@ function CreateGroup() {
       />
       <Row justify="center">
         <Col lg={8}>
-          <Form className="form" layout="vertical" size="large">
-            <Form.Item label="Name">
+          <Form className="form" form={form} layout="vertical" size="large">
+            <Form.Item name="name" label="Name">
               <Input placeholder="Name" />
             </Form.Item>
-            <Form.Item label="Description">
+            <Form.Item name="description" label="Description">
               <Input placeholder="Description" />
+            </Form.Item>
+            <Form.Item name="logo" label="Logo">
+              <Input placeholder="http://" />
             </Form.Item>
           </Form>
         </Col>
       </Row>
       <Row justify="center">
         <Col>
-          <Link to="/">
-            <Button className="button" type="primary">
-              Submit
-            </Button>
+          <Link to="/" onClick={addGroup}>
+          <Button className="button" type="primary">
+            Submit
+          </Button>
           </Link>
         </Col>
       </Row>
--- client/src/components/GroupList/GroupList.js
+++ client/src/components/GroupList/GroupList.js
@@ -1,16 +1,19 @@
 import "./GroupList.css";
 
 import Group from "../Group/Group";
+import GroupsContext from "../../context/GroupsContext";
+import { useContext } from "react";
 
-function GroupList(props) {
-  let groupsData = props.groupsData;
-  let groups = groupsData.map((groupData) => (
-    <Group group={groupData} key={groupData.groupID} />
+function GroupList() {
+  const [groups] = useContext(GroupsContext);
+  let groupsData = groups;
+  let grouplist = groupsData.map((groupData) => (
+    <Group group={groupData} key={groupData.group_id} />
   ));
 
   return (
     <div>
-      <ul>{groups}</ul>
+      <ul>{grouplist}</ul>
     </div>
   );
 }
