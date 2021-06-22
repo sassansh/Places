d8c6456d933b8356d1083635f794bd44d65d8d40
Sassan Shokoohi <sassan_shokoohi@me.com>
2021-06-22 01:15:59 -0700

    🐛 FIX: Delete Contexts

--- client/src/App.js
+++ client/src/App.js
@@ -7,8 +7,6 @@ import AddPlace from "./components/AddPlace/AddPlace";
 import AddReview from "./components/AddReview/AddReview";
 import CategoryView from "./components/CategoryView/CategoryView";
 import CreateGroup from "./components/CreateGroup/CreateGroup";
-import CurrentCategoryIDContext from "./context/CurrentCategoryIDContext";
-import CurrentPlaceIDContext from "./context/CurrentPlaceIDContext";
 import CurrentUserIDContext from "./context/CurrentUserIDContext";
 import GroupListView from "./components/GroupListView/GroupListView";
 import GroupView from "./components/GroupView/GroupView";
@@ -26,8 +24,6 @@ const { Content, Footer, Sider } = Layout;
 
 function App() {
   const currentUserID = useState(1);
-  const currentCategoryID = useState(1);
-  const currentPlaceID = useState(1);
 
   const users = useState([
     {
@@ -87,50 +83,46 @@ function App() {
           <Content style={{ margin: "24px 16px 0", overflow: "initial" }}>
             <Switch>
               <CurrentUserIDContext.Provider value={currentUserID}>
-                  <CurrentCategoryIDContext.Provider value={currentCategoryID}>
-                    <CurrentPlaceIDContext.Provider value={currentPlaceID}>
-                      <UsersContext.Provider value={users}>
-                              <ReviewsContext.Provider value={reviews}>
-                                <Route exact path="/" component={GroupView} />
-                                <Route
-                                  exact
-                                  path="/creategroup"
-                                  component={CreateGroup}
-                                />
-                                <Route
-                                  exact
-                                  path="/categoryview"
-                                  component={CategoryView}
-                                />
-                                <Route
-                                  exact
-                                  path="/addplace"
-                                  component={AddPlace}
-                                />
-                                <Route
-                                  exact
-                                  path="/placeview"
-                                  component={PlaceView}
-                                />
-                                <Route
-                                  exact
-                                  path="/addreview"
-                                  component={AddReview}
-                                />
-                                <Route
-                                  exact
-                                  path="/grouplistview"
-                                  component={GroupListView}
-                                />
-                                <Route
-                                  exact
-                                  path="/submittedReview"
-                                  component={SubmittedReview}
-                                />
-                              </ReviewsContext.Provider>
-                      </UsersContext.Provider>
-                    </CurrentPlaceIDContext.Provider>
-                  </CurrentCategoryIDContext.Provider>
+                <UsersContext.Provider value={users}>
+                  <ReviewsContext.Provider value={reviews}>
+                    <Route exact path="/" component={GroupView} />
+                    <Route
+                      exact
+                      path="/creategroup"
+                      component={CreateGroup}
+                    />
+                    <Route
+                      exact
+                      path="/categoryview"
+                      component={CategoryView}
+                    />
+                    <Route
+                      exact
+                      path="/addplace"
+                      component={AddPlace}
+                    />
+                    <Route
+                      exact
+                      path="/placeview"
+                      component={PlaceView}
+                    />
+                    <Route
+                      exact
+                      path="/addreview"
+                      component={AddReview}
+                    />
+                    <Route
+                      exact
+                      path="/grouplistview"
+                      component={GroupListView}
+                    />
+                    <Route
+                      exact
+                      path="/submittedReview"
+                      component={SubmittedReview}
+                    />
+                  </ReviewsContext.Provider>
+                </UsersContext.Provider>
               </CurrentUserIDContext.Provider>
             </Switch>
           </Content>
--- client/src/context/CurrentCategoryIDContext.js
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react";
-
-const CurrentCategoryIDContext = createContext({
-  CurrentCategoryID: 1,
-  setCurrentCategoryID: () => {},
-});
-
-export default CurrentCategoryIDContext;
