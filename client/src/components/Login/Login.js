7be98b6aa1912211de1ccf2aecde26c37a1656b2
Sassan Shokoohi <sassan_shokoohi@me.com>
2021-06-23 15:45:17 -0700

    👌 IMPROVE: Redirect after login

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -3,44 +3,49 @@ import "./Login.css";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 
+import { Redirect } from "react-router";
 import { loginUser } from "../../redux/actions/userActions";
 
 function Login() {
-  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [loginAttempts, setLoginAttempts] = useState(0);
   const dispatch = useDispatch();
   const currentUserID = useSelector((state) => state.users.currentUserID);
   const users = useSelector((state) => state.users.allUsers);
 
+  const isLoggedIn = currentUserID !== null;
+
   useEffect(() => {
     if (loginAttempts > 0) {
-      currentUserID === null ? alert("Login failed") : alert("Login succeeded");
+      currentUserID === null ? alert("Login failed") : console.log("logged in");
     }
   }, [currentUserID, loginAttempts]);
 
   // Login page inspired by: https://codepen.io/colorlib/pen/rxddKy
   function handleLogin() {
-    if (username === "" || password === "") {
-      alert("Please fill out username and password");
+    if (email === "" || password === "") {
+      alert("Please fill out email and password");
       return;
     }
-    dispatch(loginUser(username, password));
+    dispatch(loginUser(email, password));
     setLoginAttempts(loginAttempts + 1);
   }
 
   const currentUser = users.find((user) => user.user_id === currentUserID);
 
-  return (
+  return isLoggedIn ? (
+    <Redirect to={{ pathname: "/" }} />
+  ) : (
     <div className="login-page">
       Logged in user: {currentUserID ? currentUser.name : "null"}
       <div className="login-container">
         <form className="login-form">
           <input
             type="text"
-            value={username}
-            onInput={(e) => setUsername(e.target.value)}
-            placeholder="username"
+            value={email}
+            onInput={(e) => setEmail(e.target.value)}
+            placeholder="email"
           />
           <input
             type="password"
