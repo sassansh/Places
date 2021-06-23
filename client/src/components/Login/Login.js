1e43ad786b383d3cdc28d5e8037d7e13a25b1d5e
Sassan Shokoohi <sassan_shokoohi@me.com>
2021-06-23 14:53:35 -0700

    👌 IMPROVE: Login using form input

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,23 +1,32 @@
 import "./Login.css";
 
 import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 
 import { loginUser } from "../../redux/actions/userActions";
 
 function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loginAttempts, setLoginAttempts] = useState(0);
   const dispatch = useDispatch();
   const currentUserID = useSelector((state) => state.users.currentUserID);
   const users = useSelector((state) => state.users.allUsers);
 
+  useEffect(() => {
+    if (loginAttempts > 0) {
+      currentUserID === null ? alert("Login failed") : alert("Login succeeded");
+    }
+  }, [currentUserID, loginAttempts]);
+
   // Login page inspired by: https://codepen.io/colorlib/pen/rxddKy
   function handleLogin() {
-    console.log("login");
-    dispatch(loginUser("sassan_shokoohi@me.com", "1234"));
-    if (currentUserID === null) {
-      console.log("login failed");
-    } else {
-      console.log("login good");
+    if (username === "" || password === "") {
+      alert("Please fill out username and password");
+      return;
     }
+    dispatch(loginUser(username, password));
+    setLoginAttempts(loginAttempts + 1);
   }
 
   const currentUser = users.find((user) => user.user_id === currentUserID);
@@ -27,10 +36,20 @@ function Login() {
       Logged in user: {currentUserID ? currentUser.name : "null"}
       <div className="form">
         <form className="login-form">
-          <input type="text" placeholder="username" />
-          <input type="password" placeholder="password" />
+          <input
+            type="text"
+            value={username}
+            onInput={(e) => setUsername(e.target.value)}
+            placeholder="username"
+          />
+          <input
+            type="password"
+            value={password}
+            onInput={(e) => setPassword(e.target.value)}
+            placeholder="password"
+          />
           <button type="button" onClick={handleLogin}>
-            login sassan
+            login
           </button>
           <p className="message">Not registered? Create an account</p>
         </form>
