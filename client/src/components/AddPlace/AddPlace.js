d8deebaaab895eaa41fa9f2df282e27f0f9de702
Laura Rodgers <>
2021-06-24 13:53:11 -0700

    addplace goes to added place

--- client/src/components/AddPlace/AddPlace.js
+++ client/src/components/AddPlace/AddPlace.js
@@ -1,7 +1,9 @@
 import "./AddPlace.css";
 
 import { Button, Col, Divider, Form, Input, Row, Typography } from "antd";
+import { Link } from "react-router-dom";
 import { addPlace, setPlace } from "../../redux/actions/placeActions";
+import { setCategory } from "../../redux/actions/categoryActions";
 import { useDispatch, useSelector } from "react-redux";
 
 function AddPlace() {
@@ -76,9 +78,11 @@ function AddPlace() {
       </Row>
       <Row justify="center">
         <Col>
-          <Button onClick={handleAddPlace} className="button" type="primary">
-            Submit
-          </Button>
+          <Link to="/placeView" onClick={handleAddPlace}>
+            <Button className="button" type="primary">
+              Submit
+            </Button>
+          </Link>
         </Col>
       </Row>
     </div>
