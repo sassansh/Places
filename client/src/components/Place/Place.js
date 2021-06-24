22743d2ce148d686d5e9227bca937d55a46e25da
Laura Rodgers <>
2021-06-24 14:24:14 -0700

    arrayfunc in Place to get avg score

--- client/src/components/Place/Place.js
+++ client/src/components/Place/Place.js
@@ -8,16 +8,14 @@ import { setPlace } from "../../redux/actions/placeActions"
 
 function Place({ placeData, rank }) {
   const dispatch = useDispatch();
-  const reviews = useSelector((state) => state.reviews.allReviews);
-  let numRaters = 0;
-  let allRatings = 0;
-  reviews.forEach(review => {
-    if (review.place_id === placeData.place_id) {
-      numRaters++;
-      allRatings += review.rating;
-    }
-  });
-  let rating = allRatings/numRaters;
+  const reviews = useSelector((state) => state.reviews.allReviews)
+    .filter((review) => review.place_id == placeData.place_id);
+  // let numRaters = 0;
+  // let allRatings = 0;
+  const rating = reviews
+        .map((review) => review.rating)
+        .reduce((p, c) => p + c, 0) / reviews.length;
+        
   return (
     <Link to="/placeview" onClick={() => dispatch(setPlace(placeData.place_id))} >
       <Card style={{ margin: 16 }}>
@@ -30,7 +28,7 @@ function Place({ placeData, rank }) {
 
         <span className="place-rating">
           <Rate disabled allowHalf defaultValue={rating} />
-          <span className="num-of-reviews">{numRaters}</span>
+          <span className="num-of-reviews">{reviews.length}</span>
         </span>
       </Card>
     </Link>
