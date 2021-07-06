ee41337069b2bd81db7097b402e7907c7c9dd233
johnnybcs <johnny@ualberta.ca>
2021-07-06 16:52:57 -0700

    backend for Reviews

---
+++ backend/api/reviews.js
@@ -0,0 +1,47 @@
+import Review from "../models/Review.js";
+import express from "express";
+import { v4 as uuidv4 } from "uuid";
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  Review.find()
+    .then((reviews) => res.json(reviews))
+    .catch((err) => console.log(err));
+});
+
+router.post("/", (req, res) => {
+  console.log(JSON.stringify(req.body));
+  console.log("HERE!");
+  const { user_id, place_id, rating } = req.body;
+  const newReview = new Review ({
+    review_id: uuidv4(),
+    user_id: user_id,
+    place_id: place_id,
+    rating: rating,
+  });
+  newReview.save().then(() =>
+      res.json({
+        message: "Created review successfully",
+      })).catch((err) =>
+      res.status(400).json({
+        error: err,
+        message: "Error creating review"
+      })
+    );
+});
+
+router.put("/", (req, res) => {
+  const { review_id, rating } = req.body;
+  Review.updateOne({review_id: review_id}, {rating: rating}).then(() =>
+      res.json({
+        message: "Edited review successfully",
+      })).catch((err) =>
+      res.status(400).json({
+        error: err,
+        message: "Error editing review"
+      })
+    );
+});
+
+export default router;
---
+++ backend/models/Review.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const reviewSchema = new Schema({
+  review_id: {
+    type: String,
+    required: true,
+  },
+  user_id: {
+    type: String,
+    required: true,
+  },
+  place_id: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+  }
+});
+
+export default mongoose.model("Review", reviewSchema, "reviews");
