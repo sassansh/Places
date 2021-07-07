f585c22765882a459a9acf5395a9982e11bbdbe6
Laura Rodgers <>
2021-07-06 20:09:55 -0700

    has some columns

--- client/src/components/Request/Request.js
+++ client/src/components/Request/Request.js
@@ -1,26 +1,38 @@
 import "./Request.css";
 
-import { Avatar, Card, Button } from "antd";
+import { Avatar, Card, Button, Row, Col } from "antd";
 import { CheckOutlined, StopOutlined } from "@ant-design/icons";
 
 function Request(props) {
   return (
     <Card style={{ margin: 16 }}>
-      <Avatar src={props.user.avatarURL} size={64} />
-      {props.user.name + " wants to join " + props.group.name}
-      <Avatar src={props.group.avatarURL} size={64} />
-      <Button
-        icon={<CheckOutlined size="large" />}
-        size="medium"
-      >
-        Accept
-      </Button>
-      <Button
-        icon={<StopOutlined size="large" />}
-        size="medium"
-      >
-        Reject
-      </Button>
+      <Row align="middle" justify="space-around">
+        <Col>
+          <Avatar src={props.user.avatarURL} size={64} />
+          {props.user.name}
+        </Col>
+        <Col>
+          {" wants to join "}
+        </Col>
+        <Col>
+          {props.group.name}
+          <Avatar src={props.group.avatarURL} size={64} />
+        </Col>
+        <Col>
+          <Button
+            icon={<CheckOutlined size="large" />}
+            size="medium"
+          >
+            Accept
+          </Button>
+          <Button
+            icon={<StopOutlined size="large" />}
+            size="medium"
+          >
+            Reject
+          </Button>
+        </Col>
+      </Row>
     </Card>
   );
 }
