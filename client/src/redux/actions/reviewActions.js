4355e3a6db85d6e718cc8a753003d863ec37fc14
johnnybcs <johnny@ualberta.ca>
2021-07-06 21:45:22 -0700

    get Reviews frontend

--- client/src/redux/actions/reviewActions.js
+++ client/src/redux/actions/reviewActions.js
@@ -1,3 +1,23 @@
+import axios from "axios";
+import { async } from "rxjs";
+
+export const getReviews = () => async(dispatch) => {
+    try {
+        const reviewsResponse = await axios.get("/api/reviews");
+        const reviews = reviewsResponse.data;
+        dispatch(setReviews(reviews));
+      } catch (err) {
+        console.log(err);
+      }
+}
+
+export const setReviews = (reviews) => {
+    return {
+      type: "SET_REVIEWS",
+      payload: reviews,
+    };
+  };
+
 export const addReview =  newReview => {
     return {
        type: 'ADD_REVIEW',
