1dd467aa4bfecce5d01780e6eea56e1704c398d6
Amir Jafarvand <amir.jafarvand@gmail.com>
2021-07-08 08:47:56 -0700

    add user to the group just created

--- backend/api/groups.js
+++ backend/api/groups.js
@@ -1,4 +1,5 @@
 import Group from "../models/Group.js";
+import User from "../models/User.js";
 import express from "express";
 import { v4 as uuidv4 } from "uuid";
 
@@ -11,9 +12,10 @@ router.get("/", (req, res) => {
 });
 
 router.post("/", (req, res) => {
-  const { name, description, avatarURL } = req.body;
+  const { name, description, avatarURL, user_id, userGroups } = req.body;
+  let group_id = uuidv4();
   const newGroup = new Group({
-    group_id: uuidv4(),
+    group_id: group_id,
     name: name,
     description: description,
     avatarURL: avatarURL,
@@ -22,7 +24,7 @@ router.post("/", (req, res) => {
     .save()
     .then(() =>
       res.json({
-        group: newGroup,
+        group_id: group_id,
         message: "Added group successfully",
       })
     )
@@ -32,6 +34,12 @@ router.post("/", (req, res) => {
         message: "Error adding group",
       })
     );
+  userGroups.push(group_id);
+  User.updateOne({user_id: user_id}, {groups: userGroups})
+    .then((user) => res.json(user))
+    .catch((err) => console.log(err));
+
+  
 });
 
 export default router;
