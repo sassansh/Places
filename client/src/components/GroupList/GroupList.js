695d86b14cd142d314d6ca2a8c5ef14d6babf96d
Sassan Shokoohi <sassan_shokoohi@me.com>
2021-07-13 21:02:02 -0700

    👌 IMPROVE: GroupListView shows relevant button

--- client/src/components/GroupList/GroupList.js
+++ client/src/components/GroupList/GroupList.js
@@ -1,15 +1,14 @@
-import "./GroupList.css";
+import './GroupList.css';
 
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
-import { Col, Row } from "antd";
+import { Col, Row } from 'antd';
+import { useDispatch, useSelector } from 'react-redux';
 
-import Group from "../Group/Group";
-import { Link } from "react-router-dom";
-import { getGroups, setCurrentGroup } from "../../redux/actions/groupActions";
-import { getReviews } from "../../redux/actions/reviewActions";
-import { getPlaces } from "../../redux/actions/placeActions";
-import { getUsers } from "../../redux/actions/userActions";
+import Group from '../Group/Group';
+import { getGroups } from '../../redux/actions/groupActions';
+import { getPlaces } from '../../redux/actions/placeActions';
+import { getReviews } from '../../redux/actions/reviewActions';
+import { getUsers } from '../../redux/actions/userActions';
+import { useEffect } from 'react';
 
 function GroupList() {
   const dispatch = useDispatch();
@@ -24,15 +23,7 @@ function GroupList() {
   const groups = useSelector((state) => state.groups.allGroups);
   let groupsData = groups;
   let grouplist = groupsData.map((groupData) => (
-    <Link
-      to="/groupview"
-      onClick={() => {
-        dispatch(setCurrentGroup(groupData.group_id));
-      }}
-      key={groupData.group_id}
-    >
-      <Group group={groupData} key={groupData.group_id} />
-    </Link>
+    <Group group={groupData} key={groupData.group_id} />
   ));
 
   return (
