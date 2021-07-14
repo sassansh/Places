1344a499ab9ef98245e196845b6a8839408421f3
Sassan Shokoohi <sassan_shokoohi@me.com>
2021-07-13 21:24:22 -0700

    🐛 FIX: Navbar css styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,31 @@
-import "./App.less";
-import "antd/dist/antd.less";
+import './App.less';
+import 'antd/dist/antd.less';
 
 import {
   Route,
   HashRouter as Router,
   Switch,
   withRouter,
-} from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+} from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 
-import AddPlace from "./components/AddPlace/AddPlace";
-import AddReview from "./components/AddReview/AddReview";
-import CategoryView from "./components/CategoryView/CategoryView";
-import CreateGroup from "./components/CreateGroup/CreateGroup";
-import GroupListView from "./components/GroupListView/GroupListView";
-import GroupView from "./components/GroupView/GroupView";
-import { Layout } from "antd";
-import Login from "./components/Login/Login";
-import MobileNavBar from "./components/Navigation/MobileNavBar/MobileNavBar";
-import NavBar from "./components/Navigation/NavBar/NavBar";
-import PlaceView from "./components/PlaceView/PlaceView";
-import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
-import Register from "./components/Register/Register";
-import RequestView from "./components/RequestView/RequestView";
-import SideBar from "./components/Navigation/SideBar/SideBar";
-import { setCurrentUser } from "./redux/actions/userActions";
-import { useEffect } from "react";
+import AddPlace from './components/AddPlace/AddPlace';
+import AddReview from './components/AddReview/AddReview';
+import CategoryView from './components/CategoryView/CategoryView';
+import CreateGroup from './components/CreateGroup/CreateGroup';
+import GroupListView from './components/GroupListView/GroupListView';
+import GroupView from './components/GroupView/GroupView';
+import { Layout } from 'antd';
+import Login from './components/Login/Login';
+import MobileNavBar from './components/Navigation/MobileNavBar/MobileNavBar';
+import NavBar from './components/Navigation/NavBar/NavBar';
+import PlaceView from './components/PlaceView/PlaceView';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+import Register from './components/Register/Register';
+import RequestView from './components/RequestView/RequestView';
+import SideBar from './components/Navigation/SideBar/SideBar';
+import { setCurrentUser } from './redux/actions/userActions';
+import { useEffect } from 'react';
 
 const { Content, Footer } = Layout;
 
@@ -48,19 +48,23 @@ function App() {
   return (
     <Router basename="/">
       {isAuthenticated && <MobileNavBar menu={<NavBarWithRouter />} />}
-      <Layout style={{ marginTop: "60px" }}>
+      <Layout style={{ marginTop: '62px' }}>
         {isAuthenticated && <SideBar menu={<NavBarWithRouter />} />}
         <Layout
           className="site-layout"
-          style={!isAuthenticated ? { marginLeft: "0px" } : {}}
+          style={!isAuthenticated ? { marginLeft: '0px' } : {}}
         >
-          <Content style={{ margin: "24px 16px 0", overflow: "initial" }}>
+          <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
             <Route exact path="/login" component={Login} />
             <Route exact path="/register" component={RegisterWithRouter} />
             <Switch>
               <PrivateRoute exact path="/" component={GroupListView} />
               <PrivateRoute exact path="/groupview" component={GroupView} />
-              <PrivateRoute exact path="/creategroup" component={CreateGroupWithRouter} />
+              <PrivateRoute
+                exact
+                path="/creategroup"
+                component={CreateGroupWithRouter}
+              />
               <PrivateRoute
                 exact
                 path="/categoryview"
@@ -72,7 +76,7 @@ function App() {
               <PrivateRoute exact path="/requestview" component={RequestView} />
             </Switch>
           </Content>
-          <Footer style={{ textAlign: "center" }}>
+          <Footer style={{ textAlign: 'center' }}>
             Places App ©2021 Created by Team Green 🌱
           </Footer>
         </Layout>
