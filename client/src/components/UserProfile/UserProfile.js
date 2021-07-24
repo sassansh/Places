e21816c9bd661090d5592502e63c90fcf345de12
johnnybcs <johnny@ualberta.ca>
2021-07-24 00:24:05 -0700

    user profile working

--- client/src/components/UserProfile/UserProfile.js
+++ client/src/components/UserProfile/UserProfile.js
@@ -1,38 +1,50 @@
 import {
-    Title,
     Avatar,
-    Button,
     Col,
     Divider,
-    Image,
     Rate,
     Row,
     Typography,
   } from 'antd';
 import { useSelector } from 'react-redux';
 import './UserProfile.css';
-import { useEffect, useRef, useState } from 'react';
-
 
 function UserProfile() {
     const { Title } = Typography;
-
     const user = useSelector((state) => state.users.user);
-    const groups = useSelector((state) => state.groups.allGroups);
+    const users = useSelector((state) => state.users.allUsers);
+    const userData = users.find(item => item.user_id === user.user_id);
     const reviews = useSelector((state) => state.reviews.allReviews);
     const places = useSelector((state) => state.places.allPlaces);
+    const groups = useSelector((state => state.groups.allGroups));
 
     function getPlace(place_id) {
         let placeName = places.find(place => place.place_id === place_id).name;
         return placeName;
     }
 
-    const [myGroups, setMyGroups] = useState(groups.filter((group) => user.groups.includes(group.group_id)));
-    const myGroupsItems = myGroups.map((group) => <li>{group.name}</li>);
+    function getRating(rating) {
+        return (
+            <Rate
+            style={{ fontSize: '15px' }}
+            value={rating}
+            disabled={true}
+          />
+        );
+    }
 
-    const [myReviews, setMyReviews] = useState(reviews.filter((review) => review.user_id === user.user_id));
-    const myReviewsItems = myReviews.map((review) => <li>{getPlace(review.place_id) + ": " + review.rating}</li>);
+    function getGroup(group_id) {
+        return (
+            groups.find(group => group.group_id === group_id).name
+        );
+    }
 
+    const myGroups = userData.groups;
+    const myGroupsItems = myGroups.map((group_id) => <li>{getGroup(group_id)}</li>);
+
+    const myReviews = reviews.filter((review) => review.user_id === user.user_id);
+    const myReviewsItems = myReviews.map((review) => <li><Row justify="end"><Col className="place">{getPlace(review.place_id)}</Col><Col>{getRating(review.rating)}</Col></Row></li>);
+    
     return (
         <div className="container">
             <Row justify="left">
@@ -53,25 +65,27 @@ function UserProfile() {
                     <Col flex={1}>
                         <Avatar
                             size={200}
-                            src={user.avatarURL}
+                            src={userData.avatarURL}
                         />
                         <div className="label">
-                            {user.name}
+                            {userData.name}
                         </div>
                     </Col>                 
                 </div>
-                <Col flex={4}>
-                    <Title level={4}>
-                        My Groups
-                    </Title>
-                    <ul>{myGroupsItems}</ul>
-                    <Title level={4}>
-                        My Reviews
-                    </Title>
-                    <ul>{myReviewsItems}</ul>
-                </Col>
+                <div className="column2">
+                    <Col flex={4}>
+                        <div className="heading">
+                            My Groups
+                        </div>
+                        <ul className="groups">{myGroupsItems}</ul>
+                        <br />
+                        <div className="heading">
+                            My Reviews
+                        </div>
+                        <ul>{myReviewsItems}</ul>
+                    </Col>
+                </div>
             </Row>
-
         </div>
     );
 }
