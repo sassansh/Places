b63e9b2f0fac378bff8b94fffcf3b001a4cfe657
johnnybcs <johnny@ualberta.ca>
2021-07-27 23:53:06 -0700

    mobile friendly, groups, categories

--- client/src/components/UserProfile/UserProfile.js
+++ client/src/components/UserProfile/UserProfile.js
@@ -7,17 +7,26 @@ import {
     Typography,
   } from 'antd';
 import Avatar from 'antd/lib/avatar/avatar';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { getCategories } from '../../redux/actions/categoryActions';
 import './UserProfile.css';
 
 function UserProfile() {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(getCategories());
+      }, [dispatch]);
+
     const { Title } = Typography;
+    const { Meta } = Card;
     const user = useSelector((state) => state.users.user);
     const users = useSelector((state) => state.users.allUsers);
     const userData = users.find(item => item.user_id === user.user_id);
     const reviews = useSelector((state) => state.reviews.allReviews);
     const places = useSelector((state) => state.places.allPlaces);
     const groups = useSelector((state => state.groups.allGroups));
+    const categories = useSelector((state => state.categories.allCategories));
 
     let numGroups = 0;
     let numPlaces = 0;
@@ -47,20 +56,62 @@ function UserProfile() {
         try {
             numGroups++;
             return (
-                <Card size="small"><span className="group"><Avatar size={64} src={targetGroup.avatarURL} />&emsp;{targetGroup.name}</span></Card>
+                <Card size="small">
+                    <span className="group">
+                        <Avatar size={64} src={targetGroup.avatarURL} />&emsp;{targetGroup.name}
+                    </span>
+                </Card>
             );
         } catch (e) {
             numGroups--;
             console.error(e);
         }
+    }
 
+    function getGroupName(place_id) {
+        try {
+            let group_id = places.find(place => place.place_id === place_id).group_id;
+            let groupName = groups.find(group => group.group_id === group_id).name;
+            return " 👤 " + groupName;
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    function getCategoryName(place_id) {
+        try {
+            let category_id = places.find(place => place.place_id === place_id).category_id;
+            let categoryEmoji = categories.find(category => category.category_id === category_id).emoji;
+            let categoryName = categories.find(category => category.category_id === category_id).name;
+            return " " + categoryEmoji + " " + categoryName + " ";
+        } catch (e) {
+            console.error(e);
+        }
     }
 
     const myGroups = userData.groups;
-    const myGroupsItems = myGroups.map((group_id) => <li className="group">{getGroup(group_id)}</li>);
+    const myGroupsItems = myGroups.map((group_id) => 
+        <li className="group">
+            {getGroup(group_id)}
+        </li>);
 
     const myReviews = reviews.filter((review) => review.user_id === user.user_id);
-    const myReviewsItems = myReviews.map((review) => <li className="review"><Card size="small"><Row><Col span={12}><span className="place">{getPlace(review.place_id)}</span></Col><Col className="rating" span={12}>{getRating(review.rating)}</Col></Row></Card></li>);    
+    const myReviewsItems = myReviews.map((review) => 
+        <li className="review">
+            <Card size="small">
+                <Row>
+                    <Col span={12}>
+                        <span className="place">{getPlace(review.place_id)}</span>
+                    </Col>
+                    <Col className="rating" span={12}>
+                        {getRating(review.rating)}
+                    </Col>
+                </Row>
+                <Meta className="meta" description={getCategoryName(review.place_id) + getGroupName(review.place_id)}>
+                </Meta>
+            </Card>
+        </li>
+    );    
 
     return (
         <div className="container">
@@ -78,7 +129,7 @@ function UserProfile() {
                 }}
             />
             <Row justify="center">
-                <Col span={6} className="column1">
+                <Col className="column1" lg={6} md={6} sm={24}>
                     <div className="profile">
                         <img
                             className="profilePic"
@@ -100,13 +151,13 @@ function UserProfile() {
                     <div className="details">Member of {numGroups} Groups</div>
                     <div className="details">Reviewed {numPlaces} Places</div>
                 </Col>                 
-                <Col span={9} className="column2">
+                <Col className="column2" lg={9} md={9} sm={24}>
                     <div className="heading">
                         My Groups
                     </div>
                     <ul className="group">{myGroupsItems}</ul>
                 </Col>
-                <Col span={9} className="column3">
+                <Col className="column3" lg={9} md={9} sm={24}>
                     <div className="heading">
                         My Reviews
                     </div>
