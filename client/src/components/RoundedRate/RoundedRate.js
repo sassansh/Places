51644ef09dbf1099359ec5c00e1240614c7b2d26
Laura Rodgers <>
2021-08-08 14:44:44 -0700

    fixed quotes

--- client/src/components/RoundedRate/RoundedRate.js
+++ client/src/components/RoundedRate/RoundedRate.js
@@ -3,7 +3,6 @@ import { Rate } from 'antd';
 function RoundedRate(props) {
     const { value, ...otherProps } = props;
     const roundedValue = (Math.round(value*2)/2);
-    console.log(roundedValue);
     return <Rate allowHalf value={roundedValue} {...otherProps} />;
 }
 
